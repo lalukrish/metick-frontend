26731fbb55a5a8331e8bbe285e90e2a5267a933b
lalukrish <lalukrishnavallil01@gmail.com>
2023-10-08 12:49:06 +0530

    try

--- src/components/Dashboard/DashboardComponents/appWidgetScreen.js
+++ src/components/Dashboard/DashboardComponents/appWidgetScreen.js
@@ -1,6 +1,6 @@
+import React from "react";
 import PropTypes from "prop-types";
 import { Card, Typography, Box, useTheme } from "@mui/material";
-import React from "react";
 
 export default function AppWidgetSummary({
   title,
